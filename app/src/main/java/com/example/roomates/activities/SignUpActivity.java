86e1f94336109d1daa9ba10dc19747edee379393
ChiragNayak <chiragnayak@pop-os.localdomain>
2023-08-18 21:39:00 +0530

    initial commit

---
+++ app/src/main/java/com/example/roomates/activities/SignUpActivity.java
@@ -0,0 +1,23 @@
+package com.example.roomates.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.roomates.R;
+import com.example.roomates.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListener();
+    }
+    private void setListener(){
+        binding.textSignIn.setOnClickListener(v->onBackPressed());
+    }
+}
